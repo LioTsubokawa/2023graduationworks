1335c1192b740f0639c479a25aace876205a301b
LAPTOP-BIEB081K\riots <rio.tskw.0704@gmail.com>
2023-09-28 20:43:38 +0900

    9/28(木)授業後プッシュ。測域センサーでマウスカーソルが動くようになりました！

--- environments/index.js
+++ environments/index.js
@@ -3,17 +3,20 @@ import osc from 'osc';
 
 const udpPort = new osc.UDPPort({
   localAddress: '127.0.0.1',
-  localPort: 12001,
+  localPort: 8000,
   metadata: true,
 });
+
 const { width, height } = robot.getScreenSize();
 
 udpPort.on('message', function (oscMsg, _timeTag, info) {
   const { address, args } = oscMsg;
 
-  if (address === '/position') {
-    const [x, y] = args;
-    robot.moveMouse(width * x.value, height * y.value);
+  if (address === '/touches' && args.length > 1) {
+    const x = args[1].value;
+    const y = args[2].value;
+    robot.moveMouse(x, y);
+    // console.log(args[0].value, args[1].value);
   }
 
   console.log('An OSC message just arrived!', oscMsg);

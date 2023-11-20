a6d2b92581e44fbb01133e920e38698304fca991
Hiroki Kajiya <kajiya.hiroki@gmail.com>
2023-11-20 20:40:10 +0900

    いったん API できた

--- environments/crud/handlers/create.js
+++ environments/crud/handlers/create.js
@@ -1,18 +1,134 @@
 'use strict';
 
+const crypto = require('crypto');
+const {
+  S3Client,
+  PutObjectCommand,
+} = require('@aws-sdk/client-s3');
+const Ajv = require('ajv');
+
+const ALLOWED_ORIGINS = ['http://localhost:5500'];
+
+const s3ClientConfig = {};
+
+if (process.env.IS_OFFLINE) {
+  s3ClientConfig.forcePathStyle = true;
+  s3ClientConfig.credentials = {
+    accessKeyId: 'S3RVER', // This specific key is required when working offline
+    secretAccessKey: 'S3RVER',
+  };
+  s3ClientConfig.endpoint = 'http://localhost:4569';
+}
+
+const s3Client = new S3Client(s3ClientConfig);
+const ajv = new Ajv();
+
+// 検証スキーマを定義
+const schema = {
+  properties: {
+    name: {
+      pattern: '^[\\u3040-\\u309F\\u30FC]+$',
+      type: 'string',
+    },
+    image: {
+      pattern: '^data:image\\/(jpeg|png);base64,[A-Za-z0-9+/=]+$',
+      type: 'string',
+    },
+  },
+  required: ['name', 'image'],
+  type: 'object',
+};
+
+// バリデーション関数を作成
+const validate = ajv.compile(schema);
+// base64 にマッチする正規表現
+const base64RegExp = /^data:(.+\/(.+));base64,(.*)$/;
+
+/**
+ * putObject
+ */
+const putObject = (body, contentType, key) => {
+  return s3Client.send(new PutObjectCommand({
+    Body: body,
+    Bucket: process.env.S3_BUCKET,
+    ContentType: contentType,
+    Key: key,
+  }));
+};
+
+/**
+ * hiraganaToRomaji 
+ */
+const hiraganaToRomaji = (hiragana) => {
+  const conversionTable = {
+    'あ': 'a', 'い': 'i', 'う': 'u', 'え': 'e', 'お': 'o',
+    'か': 'ka', 'き': 'ki', 'く': 'ku', 'け': 'ke', 'こ': 'ko',
+    'さ': 'sa', 'し': 'shi', 'す': 'su', 'せ': 'se', 'そ': 'so',
+    'た': 'ta', 'ち': 'chi', 'つ': 'tsu', 'て': 'te', 'と': 'to',
+    'な': 'na', 'に': 'ni', 'ぬ': 'nu', 'ね': 'ne', 'の': 'no',
+    'は': 'ha', 'ひ': 'hi', 'ふ': 'fu', 'へ': 'he', 'ほ': 'ho',
+    'ま': 'ma', 'み': 'mi', 'む': 'mu', 'め': 'me', 'も': 'mo',
+    'や': 'ya', 'ゆ': 'yu', 'よ': 'yo',
+    'ら': 'ra', 'り': 'ri', 'る': 'ru', 'れ': 're', 'ろ': 'ro',
+    'わ': 'wa', 'を': 'wo', 'ん': 'n',
+    'が': 'ga', 'ぎ': 'gi', 'ぐ': 'gu', 'げ': 'ge', 'ご': 'go',
+    'ざ': 'za', 'じ': 'ji', 'ず': 'zu', 'ぜ': 'ze', 'ぞ': 'zo',
+    'だ': 'da', 'ぢ': 'ji', 'づ': 'zu', 'で': 'de', 'ど': 'do',
+    'ば': 'ba', 'び': 'bi', 'ぶ': 'bu', 'べ': 'be', 'ぼ': 'bo',
+    'ぱ': 'pa', 'ぴ': 'pi', 'ぷ': 'pu', 'ぺ': 'pe', 'ぽ': 'po',
+    'ぁ': 'xa', 'ぃ': 'xi', 'ぅ': 'xu', 'ぇ': 'xe', 'ぉ': 'xo',
+    'っ': 'xtu', 'ゃ': 'xya', 'ゅ': 'xyu', 'ょ': 'xyo',
+    'ー': '_',
+  };
+    
+  return hiragana.split('').map(char => conversionTable[char] || char).join('-');
+};
+
 module.exports.create = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
+  const origin = event.headers.origin;
+  const prefix = crypto.randomUUID();
+  const { name, image } = JSON.parse(event.body);
+  const valid = validate(JSON.parse(event.body));
+  const headers = {
+    'Access-Control-Allow-Origin': '*',
   };
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+  if (ALLOWED_ORIGINS.includes(origin)) {
+    headers['Access-Control-Allow-Origin'] = origin;
+    headers['Access-Control-Allow-Credentials'] = true;
+  }
+
+  try {
+    // クライアントから送られた値にバリデーションエラーがあればエラーを返して終了
+    if (!valid) {
+      throw validate.errors;
+    }
+
+    const [_, contentType, extension, base64String] = image.match(base64RegExp);
+    // 画像が保存されるパス
+    const key = `${prefix}___${hiraganaToRomaji(name)}.${extension}`;
+    // リクエストボディに設定された画像データはBase64エンコードされているので、デコードする
+    const body = Buffer.from(base64String, 'base64');
+
+    await putObject(body, contentType, key);
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        name,
+        status: 'OK',
+        url: `https://${process.env.S3_BUCKET}.s3.${process.env.REGION}.amazonaws.com/${key}`,
+      }),
+    };
+  } catch (error) {
+    console.error(error);
+    return {
+      statusCode: 400,
+      body: JSON.stringify({
+        status: 'ERROR',
+        error,
+      }),
+      headers,
+    };
+  }
 };
--- environments/crud/handlers/list.js
+++ environments/crud/handlers/list.js
@@ -1,18 +1,105 @@
 'use strict';
 
+const { S3Client, ListObjectsV2Command } = require('@aws-sdk/client-s3');
+
+const ALLOWED_ORIGINS = ['http://localhost:5500'];
+
+const s3ClientConfig = {};
+
+if (process.env.IS_OFFLINE) {
+  s3ClientConfig.forcePathStyle = true;
+  s3ClientConfig.credentials = {
+    accessKeyId: 'S3RVER', // This specific key is required when working offline
+    secretAccessKey: 'S3RVER',
+  };
+  s3ClientConfig.endpoint = 'http://localhost:4569';
+}
+
+const s3Client = new S3Client(s3ClientConfig);
+
+/**
+ * romajiToHiragana 
+ */
+const romajiToHiragana = (romaji) => {
+  const conversionTable = {
+    'a': 'あ', 'i': 'い', 'u': 'う', 'e': 'え', 'o': 'お',
+    'ka': 'か', 'ki': 'き', 'ku': 'く', 'ke': 'け', 'ko': 'こ',
+    'sa': 'さ', 'shi': 'し', 'su': 'す', 'se': 'せ', 'so': 'そ',
+    'ta': 'た', 'chi': 'ち', 'tsu': 'つ', 'te': 'て', 'to': 'と',
+    'na': 'な', 'ni': 'に', 'nu': 'ぬ', 'ne': 'ね', 'no': 'の',
+    'ha': 'は', 'hi': 'ひ', 'fu': 'ふ', 'he': 'へ', 'ho': 'ほ',
+    'ma': 'ま', 'mi': 'み', 'mu': 'む', 'me': 'め', 'mo': 'も',
+    'ya': 'や', 'yu': 'ゆ', 'yo': 'よ',
+    'ra': 'ら', 'ri': 'り', 'ru': 'る', 're': 'れ', 'ro': 'ろ',
+    'wa': 'わ', 'wo': 'を', 'n': 'ん',
+    'ga': 'が', 'gi': 'ぎ', 'gu': 'ぐ', 'ge': 'げ', 'go': 'ご',
+    'za': 'ざ', 'ji': 'じ', 'zu': 'ず', 'ze': 'ぜ', 'zo': 'ぞ',
+    'da': 'だ', 'di': 'ぢ', 'du': 'づ', 'de': 'で', 'do': 'ど',
+    'ba': 'ば', 'bi': 'び', 'bu': 'ぶ', 'be': 'べ', 'bo': 'ぼ',
+    'pa': 'ぱ', 'pi': 'ぴ', 'pu': 'ぷ', 'pe': 'ぺ', 'po': 'ぽ',
+    'xa': 'ぁ', 'xi': 'ぃ', 'xu': 'ぅ', 'xe': 'ぇ', 'xo': 'ぉ',
+    'xtu': 'っ', 'xya': 'ゃ', 'xyu': 'ゅ', 'xyo': 'ょ',
+    '_': 'ー',
+  };
+    
+  return romaji.split('-').map(segment => {
+    return segment.split('').reduce((acc, char, index, arr) => {
+      const potentialRomaji = acc.remaining + char;
+      const hiragana = conversionTable[potentialRomaji];
+
+      if (hiragana) {
+        return { result: acc.result + hiragana, remaining: '' };
+      } else if (index === arr.length - 1) {
+        return { result: acc.result + acc.remaining + char, remaining: '' };
+      } else {
+        return { ...acc, remaining: potentialRomaji };
+       }
+    }, { result: '', remaining: '' }).result;
+  }).join('');
+};
+
 module.exports.list = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
+  const origin = event.headers.origin;
+  const headers = {
+    'Access-Control-Allow-Origin': '*',
   };
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+  if (ALLOWED_ORIGINS.includes(origin)) {
+    headers['Access-Control-Allow-Origin'] = origin;
+    headers['Access-Control-Allow-Credentials'] = true;
+  }
+
+  try {
+    const output = await s3Client.send(
+      new ListObjectsV2Command({
+        Bucket: process.env.S3_BUCKET,
+        // Prefix: 'sketches/',
+        Delimiter: '/',
+        MaxKeys: 1000,
+      })
+    );
+    
+    output.Contents.sort((a, b) => a.LastModified - b.LastModified);
+
+    const images = output.Contents.map(({ Key }) => {
+      return {
+        name: romajiToHiragana(Key.split('___')[1].split('.')[0]),
+        url: `https://${process.env.S3_BUCKET}.s3.${process.env.REGION}.amazonaws.com/${Key}`
+      };
+    });
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        status: 'OK',
+        images,
+      }),
+    };
+  } catch (error) {
+    console.error(error);
+    return {
+      body: JSON.stringify({ error }),
+      statusCode: 400,
+    };
+  }
 };
